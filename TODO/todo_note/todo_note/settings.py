edf7be03cce591d9d5a5d89cdf47c104a9b0963c
SmoktoK <smokotoff@yandex.ru>
2022-03-31 21:59:46 +0300

    lesson_5_bug_fix

--- TODO/todo_note/todo_note/settings.py
+++ TODO/todo_note/todo_note/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6ykhriif4@f($4gsama6on&(9_q^gwja8v=*+-b!+5va%6z6-_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
