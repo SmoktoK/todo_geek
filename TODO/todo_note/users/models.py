f280d65c8f28ec2e87e0abdef8801b27bb572186
SmoktoK <smokotoff@yandex.ru>
2022-04-07 14:37:09 +0300

    lesson_7

--- TODO/todo_note/users/models.py
+++ TODO/todo_note/users/models.py
@@ -1,13 +1,15 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
+from django.db import models
 
 
 class User(AbstractUser):
+    """
+    A standard User model from an AbstractUser to inherit the default fields from the latter
+    """
     email = models.EmailField(unique=True)
 
+    class Meta:
+        ordering = ['pk']
 
-    # USERNAME_FIELD = 'email'
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    def __str__(self):
+        return f'{self.username} | {self.email} | {self.first_name} {self.last_name}'
